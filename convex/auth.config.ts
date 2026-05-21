9f86071d7675f0d4922caf81394d9e1b9b334fe7
samolubukun <samuelolubukun@gmail.com>
2026-05-22 00:44:52 +0100

    Configure Convex authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://api.stack-auth.com/api/v1/projects/86e96f46-799f-492b-b9bd-1ebb86d2d771",
+      applicationID: "86e96f46-799f-492b-b9bd-1ebb86d2d771",
+    },
+  ],
+};
